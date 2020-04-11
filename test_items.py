cff43e48db87574c0b3ea03689c2e368082ef59f
AiratZa <zalyaliev.evermore@gmail.com>
2020-04-11 16:50:05 +0300

    For peer-to-peer check

---
+++ test_items.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+# import time
+
+def test_has_add_to_basket_button(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    browser.get(link)
+    # time.sleep(30)
+    browser.implicitly_wait(5)
+    try:
+        #если не найдет ни одного элемента, то длина (len) будет равен 0
+        add_to_basket_button = browser.find_elements(By.CLASS_NAME, "bn-add-to-basket")
+    finally:
+        assert len(add_to_basket_button) > 0, "Can not find 'ADD TO BASKET' button"
